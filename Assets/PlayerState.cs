bae968ba749eb08a16ad0129ac3cbe85f1122ed1
Jinan Lakan <lakanj25@gmail.com>
2023-06-10 12:44:52 +0200

    Initial commit

--- Assets/PlayerState.cs
+++ Assets/PlayerState.cs
@@ -7,18 +7,24 @@ public class PlayerState
 {
     protected Player player;
     protected PlayerStateMachine stateMachine;
-    private string AnimBoolName;
+    private string animBoolName;
 
     public PlayerState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName)
     {
         this.player = _player;
         this.stateMachine = _stateMachine;
-        this.AnimBoolName = _animBoolName;
+        this.animBoolName = _animBoolName;
     }
 
-    public virtual void Enter();
+    public virtual void Enter() {
+        Debug.Log("I am in " + animBoolName);
+    }
 
-    public virtual void Update();
+    public virtual void Update() {
+        Debug.Log("I am in " + animBoolName);
+    }
 
-    public virtual void Exit();
+    public virtual void Exit() { 
+        Debug.Log("I am in " + animBoolName);
+    }
 }
