7ab27f6ac51e7fcbc7e4ada278c6535d3a090176
Jinan Lakan <lakanj25@gmail.com>
2023-06-10 20:23:42 +0200

    Add logic to change player orientation

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -17,6 +17,9 @@ public class Player : MonoBehaviour
     [SerializeField] private float wallCheckDistance;
     [SerializeField] private LayerMask whatIsGround;
 
+    public int orientation {  get; private set; }
+    private bool right = true;
+
     public PlayerStateMachine stateMachine { get; private set; }
     public PlayerIdleState idleState { get; private set; }
     public PlayerMoveState moveState {get; private set;}
@@ -54,6 +57,7 @@ public class Player : MonoBehaviour
     public void SetVelocity(float _xVelocity, float _yVelocity)
     {
         rb.velocity = new Vector2(_xVelocity, _yVelocity);
+        FlipController(_xVelocity);
     }
 
     public bool IsGroundDetected() => Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance,whatIsGround);
@@ -62,6 +66,26 @@ public class Player : MonoBehaviour
     {
         Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
         Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y));
+    }
+
+    public void Flip()
+    {
+        orientation = orientation * -1;
+        right = !right;
+        transform.Rotate(0, 180, 0);
+    }
+
+    public void FlipController(float _x)
+    {
+        if(_x > 0 && !right)
+        {
+
+            Flip();
+            
+        }  else if(_x < 0 && right)
+        {
+            Flip();
+        } 
     }
 
 }
