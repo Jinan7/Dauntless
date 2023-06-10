7c2e022abac7a4a9e755fe6964d2c7c5e14b2370
Jinan Lakan <lakanj25@gmail.com>
2023-06-10 17:36:10 +0200

    Create and Initialize rigid body object

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -7,6 +8,8 @@ public class Player : MonoBehaviour
     public PlayerStateMachine stateMachine { get; private set; }
     public PlayerIdleState idleState { get; private set; }
     public PlayerMoveState moveState {get; private set;}
+
+    public Rigidbody2D rb { get; private set;}
 
     public Animator anim { get; private set; }
 
@@ -22,6 +25,7 @@ public class Player : MonoBehaviour
     private void Start()
     {
         anim = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody2D>();
         stateMachine.Initialize(idleState);
         
     }
@@ -29,6 +33,11 @@ public class Player : MonoBehaviour
     private void Update()
     {
         stateMachine.currentState.Update();
+    }
+
+    public void SetVelocity(float _xVelocity, float _yVelocity)
+    {
+        rb.velocity = new Vector2(_xVelocity, _yVelocity);
     }
 
 }
